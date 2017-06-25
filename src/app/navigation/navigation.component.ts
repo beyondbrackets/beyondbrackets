225a0231405d8c3db6de3ec3a8c8c3330f580ea9
beyondbrackets <admin@beyondbrackets.io>
2017-06-25 19:23:17 +0200

    search bar styled

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { Router, NavigationStart } from '@angular/router';
 
 @Component({
@@ -6,22 +6,30 @@ import { Router, NavigationStart } from '@angular/router';
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent implements OnInit, AfterViewInit {
   hamburger: boolean = false;
+  @ViewChild("mySearchDiv") searchDiv: ElementRef;
 
-  constructor(router:Router) {
-  router.events.subscribe(event => {
-    if(event instanceof NavigationStart) {
-      this.hamburger = false;
-    }
-    // NavigationEnd
-    // NavigationCancel
-    // NavigationError
-    // RoutesRecognized
-  });
-}
+  constructor(private router: Router, private renderer: Renderer2) {
+    router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.hamburger = false;
+      }
+    });
+  }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit(): void {
+  }
+
+  onFocus(): void {
+    this.renderer.setStyle(this.searchDiv.nativeElement, 'width', '100vw');
+    this.renderer.setStyle(this.searchDiv.nativeElement, 'right', '0');
+  }
+
+  onBlur(): void {
+    this.renderer.setStyle(this.searchDiv.nativeElement, 'width', '5rem');
+    this.renderer.setStyle(this.searchDiv.nativeElement, 'right', '5rem');  }
 }
